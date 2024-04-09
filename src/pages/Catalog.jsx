56e5087443183a606ea5d2173d054bac26872744
Sergey Crew <sergeycrew.bs@gmail.com>
2024-04-08 22:32:57 -0700

    Update Catalog.jsx

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+//import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { CardList } from "../components/CardList/CardList";
 import { Container } from "../styles/container";
