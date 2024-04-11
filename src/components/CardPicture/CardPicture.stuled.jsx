1029c9f5a470add7bdce95ca6c67c0b94df6984e
Sergey Crew <sergeycrew.bs@gmail.com>
2024-04-11 01:04:24 -0700

    update to img

--- src/components/CardPicture/CardPicture.stuled.jsx
+++ src/components/CardPicture/CardPicture.stuled.jsx
@@ -7,8 +7,11 @@ export const Wrapper = styled.div`
 `;
 
 export const Img = styled.img`
+  display: block;
+  height: 100%;
+  width: 100%;
   object-fit: cover;
-  height: 310px;
-  width: 290px;
+  // height: 310px;
+  // width: 290px;
   border-radius: 10px;
 `;
